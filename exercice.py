095dd84a0fda4123d99cf8edd84a7f4b7cf114ec
Justin Lefrançois <lefrancoisjustin@gmail.com>
2021-11-02 19:03:33 -0400

    Bruh

--- exercice.py
+++ exercice.py
@@ -7,8 +7,10 @@ PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80
 
 
 # TODO: Définissez vos fonction ici
-
-
+def comparerContenuFichier(nom_fichier1, nom_fichier2):
+    with open(nom_fichier1, "r", encoding='utf-8') as f1, open(nom_fichier2, "r", encoding='utf-8') as f2:
+        lignesF1 = f1.readlines()
+        for ligne in 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
